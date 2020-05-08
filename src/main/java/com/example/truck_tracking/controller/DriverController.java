dd68ece9d3b96e7f936ed6394af47138fa89e370
kcheruiyot <57727200+kcheruiyot@users.noreply.github.com>
2020-05-07 20:32:18 -0500

    Formatted and removed redundant code

--- src/main/java/com/example/truck_tracking/controller/DriverController.java
+++ src/main/java/com/example/truck_tracking/controller/DriverController.java
@@ -1,24 +1,24 @@
-package com.example.truck_tracking.controller;
-
-import com.example.truck_tracking.models.data.ShipmentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-/**
- * Created by Kipngetich
- */
-@Controller
-public class DriverController {
-@Autowired
-private ShipmentRepository shipmentRepository;
-    @GetMapping("driver")
-    public String index(Model model){
-        model.addAttribute("appName","Shipper's Scheduler");
-        model.addAttribute("company","Daily Shippers");
-        model.addAttribute("title","Truck Company");
-        model.addAttribute("shipments",shipmentRepository.findAll());
-        return "driver/index";
-    }
-}
+//package com.example.truck_tracking.controller;
+//
+//import com.example.truck_tracking.models.data.ShipmentRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+///**
+// * Created by Kipngetich
+// */
+//@Controller
+//public class DriverController {
+//@Autowired
+//private ShipmentRepository shipmentRepository;
+//    @GetMapping("driver")
+//    public String index(Model model){
+//        model.addAttribute("appName","Shipper's Scheduler");
+//        model.addAttribute("company","Daily Shippers");
+//        model.addAttribute("title","Truck Company");
+//        model.addAttribute("shipments",shipmentRepository.findAll());
+//        return "driver/index";
+//    }
+//}
